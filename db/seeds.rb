f9f8819f30f5d57b5d8d920caef5a003aa5e7a8d
flaviomahoney <flavio@mahoneyt.com>
2020-12-03 09:24:01 -0300

    Added first part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating"
+Restaurant.destroy_all
+User.destroy_all
+
+
+User.create!(email: "flavio@gmail.com", password: "123456")
+User.create!(email: "caique@gmail.com", password: "123456")
+
+
+restaurant1 = Restaurant.new(name: "Carretão", user: User.all.sample)
+restaurant2 = Restaurant.new(name: "Mcdonalds", user: User.all.sample)
+restaurant3 = Restaurant.new(name: "Bob's", user: User.all.sample)
+restaurant4 = Restaurant.new(name: "Burguer King", user: User.all.sample)
+
+restaurant1.save
+restaurant2.save
+restaurant3.save
+restaurant4.save
+
+puts "Created"
